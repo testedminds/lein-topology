28cf19020e032cdcfdeaf60fce6c451f217aee84
Bobby Norton <bobby@testedminds.com>
2016-09-22 05:30:13 -0500

    Ensure version number is consistent.

--- makefile
+++ makefile
@@ -3,7 +3,10 @@ SHELL := /usr/bin/env bash
 commit = `git rev-parse --short HEAD`
 
 dogfood:
+	rm -rf ~/.m2/repository/lein-topology
+	lein do test, install
 	lein topology > doc/lein-topology-$(commit).csv
+	wc -l doc/*.csv
 
 .SILENT: commit
 commit:
--- src/leiningen/topology.clj
+++ src/leiningen/topology.clj
@@ -3,7 +3,7 @@
 
 (defn topology [project & args]
   (lein/eval-in-project (update-in project [:dependencies] conj
-                                   '[lein-topology "0.1.0-SNAPSHOT"]
+                                   '[lein-topology "0.2.0-SNAPSHOT"]
                                    '[org.clojure/clojure "1.8.0"])
                         `(topology.printer/print-weighted-edges
                            (topology.edgelist/dirs->fn-edges
