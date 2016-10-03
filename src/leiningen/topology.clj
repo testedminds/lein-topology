2e52adfa087cc0f4e0a3bb846daf16bce4826a24
Bobby Norton <bobby@testedminds.com>
2016-10-03 05:16:11 -0500

    Add a docstring so that lein help is populated.

--- src/leiningen/topology.clj
+++ src/leiningen/topology.clj
@@ -1,7 +1,9 @@
 (ns leiningen.topology
   (:require [leiningen.core.eval :as lein]))
 
-(defn topology [project & args]
+(defn topology
+  "Generate the project's function dependency structure matrix."
+  [project & args]
   (lein/eval-in-project (update-in project [:dependencies] conj
                                    '[lein-topology "0.3.0-SNAPSHOT"]
                                    '[org.clojure/clojure "1.8.0"])
